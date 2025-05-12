0355026d386b4e9457e5e600f0801bf6316ad73b
Yadhurshana123 <yparameshwararaj@gmail.com>
2025-05-12 11:46:58 +0530

    changers3

--- mini_banking.py
+++ mini_banking.py
@@ -3,17 +3,17 @@ import getpass
 balance = 0
 
 def create_customer():
-    user_name = input("Enter customer name: ")
-    pass_word = getpass.getpass("Enter customer password: ")
-    nic = int(input("Enter customer NIC number: "))
-    user_ID = input("Enter customer user ID: ")
-    address = input("Enter customer address: ")
+    user_name = input("Enter customer name: ").strip()
+    pass_word = getpass.getpass("Enter customer password: ").strip()
+    nic = int(input("Enter customer NIC number: "))    
+    user_ID = input("Enter customer user ID: ").strip()
+    address = input("Enter customer address: ").strip()
     t_no = int(input("Enter customer phone number: "))
     birth_date = input("Enter customer birth date: ")
 
 
     with open("customers.txt", "a") as customer_file:
-        customer_file.write(f"{user_name}  \t  {pass_word}  \t  {user_ID}  \t  {nic}  \t  {address}  \t  {t_no}  \t  {birth_date}\n")
+        customer_file.write(f"C{user_ID}  ,  {user_name}  ,  {pass_word}  ,  {nic}  ,  {address}  ,   {t_no}  ,   {birth_date}\n")
 
     print("Customer created successfully! Now",user_name,"is a customer of Unicom TIC Bank.")
 
@@ -22,9 +22,9 @@ def create_customer():
 
 def generate_account_no():
     try:  
-        acc_num = 1000
+        acc_num = 1
         count = 0
-        with open('account_details.txt', 'r') as details_file:
+        with open('accounts.txt', 'r') as details_file:
             for line in details_file:
                 count += 1
                 acc_num += count
@@ -32,30 +32,32 @@ def generate_account_no():
 
     except FileNotFoundError:
         print("File not found")
-        return None  
+        return 
 
 def create_account():
-    user_id = input("Enter customer userID: ")
+    user_id = input("Enter customer userID:")
     with open("customers.txt", "r") as customer_file:
         for line in customer_file:
             ID = line.strip().split()
             if ID[0] == user_id:
-                ac_number = generate_account_no()
-                if ac_number is not None: 
-                    holder_name = input("Enter account holder name: ")
-                    initial_balance = float(input("Enter Initial balance :Rs."))
-                    print("Account created successfully!")
+                holder_name = input("Enter account holder name: ")
+                initial_balance = float(input("Enter Initial balance :Rs."))
+                print("Account created successfully!")
+                with open('accounts.txt', 'a') as details_file:
+                    details_file.write(f"{holder_name} , {initial_balance} , ")
+                    ac_number = generate_account_no() 
+                
+                with open('accounts.txt', 'a') as details_file:
+                    details_file.write(f"ACC{ac_number}\n")
 
-                    with open('accounts.txt', 'a') as details_file:
-                        details_file.write(f"{holder_name}\t{ac_number}\t{initial_balance}\n")
+                with open('transfer.txt', 'a') as transfer_file:
+                    transfer_file.write(f"ACC{ac_number} , Balance Rs:{initial_balance}\n")
 
-                    with open('transfer.txt', 'a') as transfer_file:
-                        transfer_file.write(f"{ac_number}\t {initial_balance}\n")
                 break
         else:
             print("User ID not found")
 
-    
+
 #{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
 
 def create_admin():
@@ -64,7 +66,7 @@ def create_admin():
     user_ID = input("Enter new admin user ID: ").strip()
     print(f"{user_name} is a new admin of UnicomTIC bank")
     with open("users.txt", "a") as user_file:
-        user_file.write(f"{user_name}\t{pass_word}\t{user_ID}\n")
+        user_file.write(f"{user_name} , {pass_word} , {user_ID}\n")
     
 #{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
 
@@ -100,7 +102,7 @@ def deposit():
         return
 
     with open('transfer.txt', 'a') as transfer_file:
-        transfer_file.write(f"{ac_number}\t Deposit Rs{amount}\n")
+        transfer_file.write(f"ACC{ac_number} , Deposit Rs{amount}\n")
 
     with open('accounts.txt', 'a') as details_file:
         details_file.write(f"Deposit Rs{amount}\n")
@@ -169,53 +171,51 @@ def account_details(account_number):
 #{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
 
 def admin_login():
-    while True:
-        admin_name = "Admin"
-        admin_password = "Admin123"
-        admin_id = "A001"
-        
-        print("Enter 1 for default admin / Enter 2 for existing admin")
+    try:
+        while True:
+            admin_name = "Admin"
+            admin_password = "Admin123"
+            admin_id = "A001"
+            
+            print("Enter 1 for default admin / Enter 2 for existing admin")
 
-        option  = input("choose an option :")
+            option  = input("choose an option :")
 
-        if option == "1":
+            if option == "1":
 
-            user_name = input("Enter your user name: ").strip()
-            pass_word = input("Enter your password: ").strip()
-            user_ID = input("Enter your user ID: ").strip()
+                user_name = input("Enter your user name: ").strip()
+                pass_word = input("Enter your password: ").strip()
+                user_ID = input("Enter your user ID: ").strip()
 
-            if user_name == admin_name and pass_word == admin_password and user_ID == admin_id :
-                print("Login Successful!")
-                print(f"Hi {user_name}, Welcome to Unicom TIC bank")
-                admin_menu() 
-                return
+                if user_name == admin_name and pass_word == admin_password and user_ID == admin_id :
+                    print("Login Successful!")
+                    print(f"Hi {user_name}, Welcome to Unicom TIC bank")
+                    admin_menu() 
+                    return
 
-            else:
-                print("Admin information not correct")
+                else:
+                    print("Admin information not correct")
 
-        elif option == "2":
-            user_name = input("Enter your user name: ").strip()
-            pass_word = input("Enter your password: ").strip()
-            user_ID = input("Enter your user ID: ").strip()
+            elif option == "2":
+                user_name = input("Enter your user name: ").strip()
+                pass_word = input("Enter your password: ").strip()
+                user_ID = input("Enter your user ID: ").strip()
 
-            with open("users.txt", "r") as user_file:
-                for user in user_file:
-                    user_details = user.strip().split()
+                with open("users.txt", "r") as user_file:
+                    for user in user_file:
+                        user_details = user.strip().split()
 
-                    if len(user_details) == 3:
-                        file_user_name, file_pass_word, file_user_ID = user_details
-                        if user_name == file_user_name and pass_word == file_pass_word and user_ID == file_user_ID:
+                        if user_name in user_details and pass_word in user_details and user_ID in user_details:
                             print("Login Successful!")
                             print(f"Hi {user_name}, Welcome Admin")
                             admin_menu()
                             return
-                        else: 
-                            print("Admin not found.Try again")
-                            break
-        else: 
-            print("Please kindly choose 1 or 2")
+            else: 
+                print("Please kindly choose 1 or 2")
 
-
+    except FileNotFoundError:
+        print("Admin data not found")                                               
+        return
 #{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
 
 
